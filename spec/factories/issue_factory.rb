2142ca367c85ea8eacff5c557205b4a48ebb2eab
Matt Holloway <mholloway@book.com>
2016-12-16 16:57:38 -0500

    Show issues

---
+++ spec/factories/issue_factory.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :issue do
+    priority "P1"
+    subject "Caliber is down"
+    issue "Uh, Houston... something is wrong"
+    impact "Major bummer"
+    status "Noob"
+  end
+end
---
+++ spec/features/viewing_issues_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+feature "Users can view issues" do 
+  before do
+    caliber = FactoryGirl.create(:platform, name: 'Caliber')
+    FactoryGirl.create(:issue, platform: caliber, priority:'P1', subject: 'Caliber is down', issue: 'Caliber is dahn', impact: 'this is bad', status:'new')
+
+    sap = FactoryGirl.create(:platform, name: 'SAP')
+    FactoryGirl.create(:issue, platform: sap, priority:'P3', subject: 'SAP is slow', issue: 'peeps cannnot see reports', impact: 'this is not terrible', status:'in process')
+
+    visit "/"
+  end
+
+  scenario 'for a given platform' do
+    click_link 'Caliber'
+
+    expect(page).to have_content "Caliber is down"
+    expect(page).to_not have_content "SAP is slow"
+
+    click_link "Caliber is down"
+    within("#issue h2") do
+      expect(page).to have_content "Caliber is down"
+    end
+
+    expect(page).to have_content "Caliber is dahn"
+  end
+end
