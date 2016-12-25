9a01ca80d793d38cabfaaad7ca72e2371fec2fd5
Matt Holloway <mholloway@book.com>
2016-12-25 18:21:15 -0500

    Add ability to set user as admin

--- spec/features/admin/creating_users_spec.rb
+++ spec/features/admin/creating_users_spec.rb
@@ -17,5 +17,23 @@ feature "Admins can create users" do
     click_button "Create User"
     expect(page).to have_content "User has been created."
   end
-  
+
+  scenario "with invalid attributes" do
+    fill_in "Email", with: ''
+    fill_in "Password", with: ''
+    click_button "Create User"
+    expect(page).not_to have_content "User has been created."
+    expect(page).to have_content "User has not been created."
+  end
+
+  scenario "setting a user to admin" do
+    fill_in "Email", with: "admin@gmail.com"
+    fill_in "Password", with: "password"
+    check "Is an Admin?"
+    click_button "Create User"
+    expect(page).to have_content "User has been created."
+    expect(page).to have_content "admin@gmail.com (Admin)"
+  end
+
+
 end
