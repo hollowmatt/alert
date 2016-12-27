e28498921c747a9e9b3d42ffddd30529fe4a7c5a
Matt Holloway <mholloway@book.com>
2016-12-26 19:03:05 -0500

    Implement graceful handling of authorization error

--- spec/controllers/platform_controller_spec.rb
+++ spec/controllers/platform_controller_spec.rb
@@ -7,4 +7,15 @@ describe PlatformsController do
     message = "The platform you were looking for could not be found."
     expect(flash[:alert]).to eq message
   end
+
+  it "handles permission errors by redirecting to a safe place" do 
+    allow(controller).to receive(:current_user)
+    platform = FactoryGirl.create(:platform)
+    get :show, id: platform
+
+    expect(response).to redirect_to root_path
+    message = "You don't have permission to perform that action."
+    expect(flash[:alert]).to eq message
+  end
+
 end
