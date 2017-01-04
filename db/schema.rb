57c48da1478169bd2a76e2ab1c0ed2e8751d3657
Matt Holloway <hollowmatt@gmail.com>
2017-01-03 22:46:51 -0500

    Change some format look/feel

--- db/schema.rb
+++ db/schema.rb
@@ -28,12 +28,12 @@ ActiveRecord::Schema.define(version: 20170101234702) do
     t.text     "issue"
     t.text     "impact"
     t.string   "ticket"
-    t.text     "status"
     t.text     "next_steps"
     t.integer  "platform_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "author_id"
+    t.string   "status"
   end
 
   add_index "issues", ["author_id"], name: "index_issues_on_author_id"
