cfe3977e8d11a078121edd7b45f66dad472983e5
Matt Holloway <mholloway@book.com>
2017-02-02 17:06:05 -0500

    Now can add users to distlist

--- app/controllers/admin/distlists_controller.rb
+++ app/controllers/admin/distlists_controller.rb
@@ -6,6 +6,23 @@ class Admin::DistlistsController < ApplicationController
     @distlists = Distlist.all
   end
 
+  def new
+    @distlist = Distlist.new
+    authorize @distlist, :new?
+  end
+
+  def create
+    @distlist = Distlist.new(distlist_params)
+    authorize @distlist, :create?
+    if @distlist.save
+      flash[:notice] = "#{@distlist.follower.name} has been added to distlist"
+      redirect_to admin_distlists_path
+    else
+      flash.now[:alert] = "Distlist has not been modified"
+      render 'new'
+    end
+  end
+
   def platdex
     @distlists = Distlist.where(platform_id: params[:id]).order("priority_id asc")
     authorize @distlists, :platdex?
@@ -18,7 +35,7 @@ class Admin::DistlistsController < ApplicationController
 
   private
     def distlist_params
-      params.require(:distlist).permit(:priority, :platform, :follower)
+      params.require(:distlist).permit(:priority_id, :platform_id, :follower_id)
     end
 
     def set_distlist
--- spec/features/admin/creating_distlist_spec.rb
+++ spec/features/admin/creating_distlist_spec.rb
@@ -2,22 +2,23 @@ require "spec_helper"
 
 feature "Admins can create distlists" do
 	let(:admin) { FactoryGirl.create(:user, :admin) }
-	let(:priority) { FactoryGirl.create(:priority) }
-	let(:platform) { FactoryGirl.create(:platform) }
-	let(:follower) { FactoryGirl.create(:follower) }
 
 	before do
 		login_as(admin)
+		FactoryGirl.create(:priority, name: 'P1')
+		FactoryGirl.create(:platform, name: 'NOOK') 
+		FactoryGirl.create(:follower) 
+
 		visit admin_distlists_path
-		click_link "Add Follower to Distlist"
+		click_link "Add to list"
 	end
 
-	xit "with valid attributes" do 
+	it "has valid attributes" do 
+		select "NOOK", from: "Platform"
 		select "P1", from: "Priority"
-		select "NOOK Dot Com", from: "Platform"
 		select "Joe Blow", from: "Follower"
-		click_button "Add to dist list"
+		click_button "Add to Distlist"
 
-		expect(page).to have_content "#{follower.name} has been added to distlist"
+		expect(page).to have_content "Joe Blow has been added to distlist"
 	end
 end
